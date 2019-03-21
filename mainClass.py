e9c3a030f7aed6630c1e7e61297d550d408e2d52
Sergey S. Leleko <sergey@leleko.ru>
2019-03-21 09:21:35 +0500

    create my first python classes and test it

---
+++ mainClass.py
@@ -0,0 +1,7 @@
+class FirstTest:
+    def test():
+        return("Function test in class FirstTest is work")
+
+class SecondTest:
+    def test2():
+        return("Function test2 in class SecondTest is work")
