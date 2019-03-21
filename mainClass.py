343ec851359025d6f0cf76dd5e1be65dd8d0bc37
Sergey S. Leleko <sergey@leleko.ru>
2019-03-21 11:59:10 +0500

    fixed

--- mainClass.py
+++ mainClass.py
@@ -1,7 +1,10 @@
 class FirstTest:
-    def test():
+    def __init__(self, arg1, arg2):
+        print(arg1 + " This is arg1 of seld method")
+
+    def test(self):
         print("Function test in class FirstTest is work")
 
 class SecondTest:
-    def test2():
+    def test2(self):
         print("Function test2 in class SecondTest is work")
