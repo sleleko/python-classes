e2aa3496c9778225094fe8e121806bc2c8090351
Sergey S. Leleko <sergey@leleko.ru>
2019-03-21 15:58:44 +0500

    clear not used code

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ class FirstTest:
 
     def test(self):
         message = (self.arg1 + " - this is arg1!")
+        '''
         message2 = (self.arg2 + " - this is arg2")
+     
         if self.arg2 != '':
             print("FirstTest class in runnging: " + message + " and " + message2)
         else:
-            print("FirstTest class in runnging: " + message)
+        '''
+        print("FirstTest class in runnging: " + message)
 
 class SecondTest(FirstTest):
     def __init__(self, arg1, arg2, arg3):
