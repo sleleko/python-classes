b441dd4562e1871a10ff378901cfe57f8392076b
Sergey S. Leleko <sergey@leleko.ru>
2019-03-21 16:25:18 +0500

    fix and rewrite message code with format()

--- main.py
+++ main.py
@@ -6,22 +6,23 @@ class FirstTest:
 
     def test(self):
         message = (self.arg1 + " - this is arg1!")
-        '''
+
         message2 = (self.arg2 + " - this is arg2")
-     
-        if self.arg2 != '':
-            print("FirstTest class in runnging: " + message + " and " + message2)
-        else:
-        '''
-        print("FirstTest class in runnging: " + message)
+
+        print("FirstTest class in runnging: " + message + " and " + message2)
+
 
 class SecondTest(FirstTest):
     def __init__(self, arg1, arg2, arg3):
         '''See on new arg3 parametr, I, add him in this new class, what was extended from parent FirstClass'''
         super().__init__(arg1, arg2)
         self.arg3 = arg3
+
     def test2(self):
         message3 = self.arg3
 
-        print("New SecondTest class calling arg1: " + self.arg1 + ", arg2: " + self.arg2 + " from FirstClass from method test and arg3: "
-              + message3 + "from SecondTest class , test2() method")
+        finalmsg = "New SecondTest class calling arg1: {argm1}, arg2: {argm2} from " \
+                   "FirstClass from method test and arg3: {argm3} " \
+                   "from SecondTest class , test2() method".format(argm1=self.arg1, argm2=self.arg2, argm3=message3)
+
+        print(finalmsg)
--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import main
 
-#Two = main.FirstTest("OnlyOneTestArgument")
+# Two = main.FirstTest("OnlyOneTestArgument")
 
 First = main.FirstTest("TestArgument1", "TestArgument2")
 Second = main.SecondTest("TestArgument1", "TestArgument2", "TestArgument3")
 
 First.test()
 Second.test2()
-#Two.test()
+# Two.test()
