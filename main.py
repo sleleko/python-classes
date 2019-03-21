ace8ac4c1d6bc56944b3c1e0dfedb3c981658c4e
Sergey S. Leleko <sergey@leleko.ru>
2019-03-21 10:11:09 +0500

    import class from modul

---
+++ main.py
@@ -0,0 +1,5 @@
+import mainClass
+First = mainClass.FirstTest().test
+Second = mainClass.SecondTest().test2
+print(First)
+print(Second)
--- mainClass.py
+++ mainClass.py
@@ -1,7 +1,7 @@
 class FirstTest:
     def test():
-        return("Function test in class FirstTest is work")
+        print("Function test in class FirstTest is work")
 
 class SecondTest:
     def test2():
-        return("Function test2 in class SecondTest is work")
+        print("Function test2 in class SecondTest is work")
